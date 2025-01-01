7e08bca1069a4f4fc6da1e150a1fd75ed181dc77
Shubham Patil <shubhamspatilnbr@gmail.com>
2025-01-02 00:42:37 +0530

    add data for live chats and import chat slice in redux store

--- src/utils/data.js
+++ src/utils/data.js
@@ -2,6 +2,14 @@
 export const commentsData = [
   {
     image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROyxOmbRVxRw6NxmEzS9fzTWl-5qhElKZOQw&s",
+    name: "SiuuTube",
+    text: "In a Live video: If the number of live chats exceeds 30, the top 10 chats are removed from the Redux store and DOM to prevent page freeze and improve performance.",
+    replies: [],
+  },
+
+  {
+    image:
       "https://c.ndtvimg.com/2024-10/l50qk0ko_mohammed-siraj_625x300_12_October_24.jpg?im=FeatureCrop,algorithm=dnn,width=806,height=605",
     name: "DSP Siraj",
     text: "I am only believe on Jassi bhai becoz Game Changer player he is, only 1 guy Jasprit Bumrah.",
@@ -100,3 +108,89 @@ export const commentsData = [
     ],
   },
 ];
+
+// Random names and comments for live chats
+export const generateRandomDataForLiveChats = () => {
+  const dummyData = [
+    {
+      name: "DSP SIraj",
+      message: "I am only believe on Jassi bhai...",
+    },
+    {
+      name: "ReactJS",
+      message: "I use Virtual DOM for performance..",
+    },
+    {
+      name: "RTK",
+      message: "I centralize state in one store 😎.",
+    },
+    {
+      name: "RTK",
+      message: "I define actions, reducers, and dispatch.",
+    },
+    {
+      name: "ReactJS",
+      message: "I use hooks for state management.",
+    },
+    {
+      name: "DSP SIraj",
+      message: "I am only believe on Jassi bhai...",
+    },
+    {
+      name: "ReactJS",
+      message: "I use React Router for navigation.",
+    },
+    {
+      name: "Tailwind",
+      message: "I am a utility-first CSS framework.",
+    },
+    {
+      name: "Tailwind",
+      message: "I avoid pre-designed components.",
+    },
+    {
+      name: "SiuuTube",
+      message: "I am built with live YT API 😎.",
+    },
+    {
+      name: "SiuuTube",
+      message: "I am using RTK for state management.",
+    },
+    {
+      name: "API Polling",
+      message: "I am using setInterval for live chat polling.",
+    },
+    {
+      name: "API Polling",
+      message: "I fetch data periodically.",
+    },
+    {
+      name: "DSP SIraj",
+      message: "I am only believe on Jassi bhai...",
+    },
+    {
+      name: "SiuuTube",
+      message: "I am using debouncing in search inputs.",
+    },
+    {
+      name: "SiuuTube",
+      message: "I support dark and light themes.",
+    },
+    {
+      name: "Debouncing",
+      message: "I reduce unnecessary API calls.",
+    },
+    {
+      name: "Tailwind",
+      message: "All are rely on my easy-to-use class names 😎.",
+    },
+    {
+      name: "DSP SIraj",
+      message: "I am only believe on Jassi bhai...",
+    },
+  ];
+
+  return {
+    ...dummyData[Math.floor(Math.random() * dummyData.length)],
+  };
+};
