b43399de941bf89840cd1d919e107c588c72724f
Shubham Patil <shubhamspatilnbr@gmail.com>
2025-01-03 18:12:35 +0530

    add search results api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,15 @@
-export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${
+const PROXY_URL = "https://corsproxy.io/?";
+
+export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=10&regionCode=IN&key=${
   import.meta.env.VITE_YOUTUBE_API_KEY
 }`;
 
-export const YOUTUBE_LIVE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&eventType=live&maxResults=25&q=news&type=video&key=${
+export const YOUTUBE_LIVE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&eventType=live&maxResults=10&type=video&key=${
   import.meta.env.VITE_YOUTUBE_API_KEY
-}`;
+}&q=news`;
 
-export const YOUTUBE_SEARCH_SUGGESTIONS_API =
-  "https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
+export const YOUTUBE_SEARCH_SUGGESTIONS_API = `${PROXY_URL}http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=`;
+
+export const YOUTUBE_VIDEO_SEARCH_RESULTS_API = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=7&safeSearch=moderate&key=${
+  import.meta.env.VITE_YOUTUBE_API_KEY
+}&q=`;
