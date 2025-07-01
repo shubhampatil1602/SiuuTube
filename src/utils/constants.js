820bda73d7c5ab822c940d133df77bdb9d077b69
Shubham Patil <shubhamspatilnbr@gmail.com>
2025-07-02 00:57:41 +0530

    add proxy for search suggestion

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const PROXY_URL = "https://thingproxy.freeboard.io/fetch/";
+// const PROXY_URL = "https://thingproxy.freeboard.io/fetch/";
 
 export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key=${
   import.meta.env.VITE_YOUTUBE_API_KEY
@@ -12,7 +12,7 @@ export const WATCH_PAGE = `https://youtube.googleapis.com/youtube/v3/videos?part
   import.meta.env.VITE_YOUTUBE_API_KEY
 }&id=`;
 
-export const YOUTUBE_SEARCH_SUGGESTIONS_API = `${PROXY_URL}http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=`;
+export const YOUTUBE_SEARCH_SUGGESTIONS_API = `https://cors-handlers.vercel.app/api/?url=http%3A%2F%2Fsuggestqueries.google.com%2Fcomplete%2Fsearch%3Fclient%3Dyoutube%26ds%3Dyt%26client%3Dfirefox%26q%3D`;
 
 export const YOUTUBE_VIDEO_SEARCH_RESULTS_API = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=7&safeSearch=moderate&key=${
   import.meta.env.VITE_YOUTUBE_API_KEY
